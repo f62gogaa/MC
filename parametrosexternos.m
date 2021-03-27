5c927b685afafd5af93db1f78cd2b59be1b91b3b
Antonia <f62gogaa@uco.es>
2021-03-27 19:10:47 +0100

    Definido como función

--- parametrosexternos.m
+++ parametrosexternos.m
@@ -1,18 +1,27 @@
-%Defino parametros externos.
-%Presion en Torr
-P=input('Presion del sistema (en Torr)');
-%Temperatura en Kelvin
-T=input('Temperatura del sistema (en Kelin)');
-%La energía va en eV.
-E=input('Valor del campo electrico aplicado(vector fila, en eV)');
-%Es la misma posición inicial para todas las partículas.
-r0=input('Posicion inicial (vector fila)');
-%Para todas las partícula será el mismo tiempo de inicio.
-t0=input('instante inicial (vector fila)');
-%Numero de particulas que participan en mi simulacion.
-part=input('Número de partículas en la simulación');
-%Energía máxima que va a poder tener mi sistema.
-E_max0=input('Energia máxima de mi sistema');
+function [P,T,E,r0,t0,E_max0,part,R,inter_max]=parametrosexternos()
+    %Uso clear para limpiar el WORKspace de variables guardadas.
+    clear
+    %Defino parametros externos.
+    %Presion en Torr
+    P=input('Presion del sistema (en Torr)');
+    %Temperatura en Kelvin
+    T=input('Temperatura del sistema (en Kelin)');
+    %El valor del campo electrico va en eV.
+    E=input('Valor del campo electrico aplicado(vector fila, en eV)');
+    %Es la misma posición inicial para todas las partículas.
+    r0=input('Posicion inicial (vector fila)');
+    %Para todas las partícula será el mismo tiempo de inicio.
+    t0=input('instante inicial');
+    %Numero de particulas que participan en mi simulacion.
+    part=input('Número de partículas en la simulación');
+    %Energía máxima que va a poder tener mi sistema.
+    E_max0=input('Energia máxima de mi sistema');
+    %Constante de los gases ideales en eV/molK.
+    R=5.1899e19;
+    %Numero maximo de interacciones.
+    inter_max=input('numero maximo de interaciones');
+end
+
 
 
     
