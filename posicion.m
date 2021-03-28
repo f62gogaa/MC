e9c963b47e71f8ed8d7e3a77a9c5e2d05eae7ea5
Antonia <f62gogaa@uco.es>
2021-03-28 12:55:13 +0200

    Primer arreglo
    Necesito arreglar el tema de k

--- posicion.m
+++ posicion.m
@@ -1,8 +1,12 @@
-function [r,v]=posicion(r0,v0,t,F,m)
+function [r,v]=posicion(r,v,t,F,m,part)
 %Esta funcion calcula la posicion y la velocida en el instante t
-%v0,r0,F deben ser definidas como vectores en horizontal v=[ ]
-    for i=1:1:3
-        r(:,i)=r0(:,i)+v0(:,i)*+0.5*(F(:,i)/m)*t^2;
-        v(:,i)=v0(:,i)+(F(:,1)/m)*t;
+%v,r contiene la información de todas las particulas de la simulación
+%BUCLE PARTICULA
+    for k=1:1:part
+%BUCLE COMPONENTE
+        for i=1:1:3
+        r(i,k)=r(i,k)+v(i,k)*t+0.5*(F(:,i)/m)*t^2;
+        v(i,k)=v(i,k)+(F(:,i)/m)*t;
+        end
     end
 end
