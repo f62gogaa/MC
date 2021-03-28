2963ba1bf1e964d4df586cd6bed363e3096f7758
Antonia <f62gogaa@uco.es>
2021-03-28 11:23:18 +0200

    Correcciones programa

--- t.m
+++ t.m
@@ -1,6 +1,9 @@
 function [t,max_seccion]=t(E_v,n,v)
 %Calculo el valor de la seccion eficaz de cada colision para los valores
 %del vector de E
+%Defino un vector que tenga las dimensiones que le corresponden a s1 y s2.
+s1=zeros(1,length(E_v));
+s2=zeros(1,length(E_v));
     for j=1:1:length(E_v)
         s1(:,j)=seccion1(E_v(:,j));
         s2(:,j)=seccion2(E_v(:,j));
@@ -10,23 +13,21 @@ sT=s1+s2;
 %Calculo el máximo de esa seccion total.
 max_seccion=max(sT);
 %Normalizo las secciones una vez conozco la maxima.
+s1_n=zeros(1,length(s1));
+s2_n=zeros(1,length(s2));
 for l=1:1:length(E_v)
-    s1_n=s1(:,l)/max_seccion;
-    s2_n=s2(:,l)/max_seccion;
+    s1_n(:,l)=s1(:,l)/max_seccion;
+    s2_n(:,l)=s2(:,l)/max_seccion;
 end
 %Vuelvo a sumar la dos secciones
 sT_n=s1_n+s2_n;
 %Calculo la velocidad media.
-v_media=mean(v);
-%Calculo de la frecuencia de colision por dato:
-frec=zeros(length(E_v),1);
-    for d=1:1:length(E_v)
-        frec(:,d)=n*v_media*sT_n(:,d);
-    end
-    %Calculo la frecuencia maxima de ese vector.
-    frec_max=max(frec);
-    %Calculo el tiempo entre colisión y colisión.
-    t=-(log(rand)/frec_max);
+v_media=mean(mean(v));
+%Calculo de la frecuencia de colision para el valor mas alto de sT_n
+max_seccion_n=max(sT_n);
+frec_max=n*v_media*max_seccion_n;
+%Ahora calculo el salto de tiempo como
+t=-(log(rand)/frec_max);
 end
 
 
