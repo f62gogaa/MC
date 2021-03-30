0103ac5dbed9c76ccb7da0842fd58e9dcf2b61e2
Antonia <f62gogaa@uco.es>
2021-03-30 12:44:33 +0200

    Funciona

--- posicion.m
+++ posicion.m
@@ -1,12 +1,18 @@
-function [r,v]=posicion(r,v,t,F,m,part)
-%Esta funcion calcula la posicion y la velocida en el instante t
-%v,r contiene la información de todas las particulas de la simulación
+function [r,v]=posicion(r_0,v0,tI,F,m,part)
+%Esta función calcula la posicion de la particula en el instante t con un
+%valor de posicion inicial fijo.
+%Predefino las dimensiones de las matriz r
+r=zeros(3,part);
+%Predefino las dimensiones de la matriz v
+v=zeros(3,part);
 %BUCLE PARTICULA
-    for k=1:1:part
-%BUCLE COMPONENTE
-        for i=1:1:3
-        r(i,k)=r(i,k)+v(i,k)*t+0.5*(F(:,i)/m)*t^2;
-        v(i,k)=v(i,k)+(F(:,i)/m)*t;
-        end
+  for k=1:1:part
+    %BUCLE COMPONENTE
+    for h=1:1:3
+        %La particula sigue un movimiento parabolico.
+        r(h,k)=r_0(h,k)+v0(h,k)*tI+(1/2)*(F(:,h)/m)*tI^2;
+        %La velocidad aumenta con la aceleración.
+        v(h,k)=v0(h,k)+(F(:,h)/m)*tI;
     end
+  end
 end
