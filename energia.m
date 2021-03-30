478e02106dff2a704474b4742132a913cfe87c30
Antonia <f62gogaa@uco.es>
2021-03-30 12:54:16 +0200

    Funciona
    cambiada de particula a particulas

--- energia.m
+++ energia.m
@@ -1,5 +1,10 @@
-function [E]=energia(v,m)
+function [E]=energia(v,m,part)
 %Esta funcion calculo el modulo de la velocidad y su energia cinetica.
-    vmodulo=norm(v);
-    E=0.5*m*vmodulo^2;
+%Comenzamos calculando el modulo de la velocidad de cada particula.
+%Predefinimos las dimensiones de los vectores vmodulo, E.
+E=zeros(1,part);
+    for k=1:1:part
+        vmodulo=norm(v(:,k));
+        E(:,k)=0.5*m*vmodulo^2;
+    end
 end
