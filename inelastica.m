0ead031401192e8580d40d095a6fffeefcef671c
Antonia <f62gogaa@uco.es>
2021-03-31 19:06:04 +0200

    Falta componentes velocidad

--- inelastica.m
+++ inelastica.m
@@ -1,10 +1,17 @@
-function [E_n,v_n]=inelastica(v,m)
+function [E_n,v_n]=inelastica(v,m,E)
+  %Calculamos el incremento de energía con respecto a la antigua.
+    inc_E=seccion2(E);
+  %Calculamos el modulo de la nueva velocidad.
+    mod_v=(norm(v)^2-(2/m)*inc_E)^(1/2);
+  %Calculamos la nueva dirección de la velocidad:
     cose(tita)=1-2*rand;
     sen(tita)=(1-cose(tita)^2)^(1/2);
     fi=2*pi*rand;
-    a=(v(:,2)^2+v(:,3)^2)^(1/2);
-    v_n(:,1)=v(:,1)*cose(tita)+a*sin(fi)*sen(tita);
-    v_n(:,2)=v(:,2)*cose(tita)-((v(:,3)*norm(v))/a)*cos(fi)*sen(tita)-((v(:,2)*v(:,1))/a)*sin(fi)*sen(tita);
-    v_n(:,3)=v(:,3)*cose(tita)+((v(:,2)*norm(v))/a)*cos(fi)*sen(tita)-((v(:,3)*v(:,1))/a)*sin(fi)*sen(tita);
-    E_n=energia(v_n,m);
+  %Calculamos las componentes de la nueva velocidad
+    v_n=zeros(3,1);
+    for h=1:1:3
+        v_n(h,:)=mod_v;
+    end
+  %Calculamos la nueva energía de la particula tras el choque:
+    E_n=(1/2)*m*mod_v^2;
 end
