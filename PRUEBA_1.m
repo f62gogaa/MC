d618298ea33455752d579bdce3951177d8d6f984
Antonia <f62gogaa@uco.es>
2021-03-31 18:00:54 +0200

    Funciona, salvo que el primer t es enorme

--- PRUEBA_1.m
+++ PRUEBA_1.m
@@ -5,30 +5,44 @@
 %han sido guardadas anteriormente.
 [P,T,E,r0,t0,E_max0,part,R,inter_max]=parametrosexternos();
 [m,M,n,F,E_v,v0,r_0]=parametrosfijos(r0,P,T,E,E_max0,part,R);
-%Incia un bucle para que se repita todo el proceso para un numero finito de
-%interacciones.
-%Predefinimos las dimensiones de las matrices que van a guardar todos los
-%datos de las interacciones.
-tT=(t0);
-
+%Inicio un bucle que vaya calculando las posiciones y velocidades de las
+%particulas en cada interacción.
 for w=1:1:inter_max
     %Primero elimino el valor de la variable t
     clear t
-    %Calculo el tiempo para la primera interaccion
     if w==1
-        [t]=t(E_v,v0);
+        %Calculo el tiempo para la primera interaccion
+        [t]=t(E_v,n,v0);
+        tT=[t0 t];
+        tI=sum(tT);
+        %Calculo la posicion y velocidad para la primera interacción.
+        [r,v]=posicion(r_0,v0,tI,F,m,part);
+        clear tI
+        %Calculo la energía de las paticulas:
+        [E_T]=energia(v,m,part);
+        %Guardamos la primera interacción:
+        R_T_I=[r];
+        V_T_I=[v];
+        %Guarda la energía media de todo el sistema
+        E_T_m=[mean(E_T)];
+        
+    else
+        %Calculamos el tiempo que avanza cada interacción, indices de los for
+        %dentro j,l
+        [t]=t(E_v,n,v);
+        tT=[tT t];
+        tI=sum(tT);
+        %Calculamos la posicion y la velocidad de las particulas.
+        [r,v]=posicion(r,v,tI,F,m,part);
+        clear tI
+        %Calculamos la energía de las particulas:
+        [E_T]=energia(v,m,part);
+        %Guarda los datos de cada interacción:
+        R_T_I=[R_T_I r];
+        V_T_I=[V_T_I v];
+        %Guarda la energia media de todo el sistema
+        E_T_m=[E_T_m mean(E_T)];  
     end
-    %Calculamos el tiempo que avanza cada interacción, indices de los for
-    %dentro j,l
-    [t]=t(E_v,n,v);
-    %Guardamos el tiempo.
-    tT=[tT t];
-    %Calculamos el tiempo total con el tiempo anterior.
-    tI=sum(tT);
-    %Calculamos la posicion y velocidad de las particulas
-    [r,v]=posicion(r_0,v0,tI,F,m,part);
-    %Calculamos la energía de cada particula.
-    [E_T]=energia(v,m,part);
 end
 %Representamos los datos en dos gráficas, una gráfica de posición y otras
 %gráfica de energía.
