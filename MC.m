60e4308038cc09577ea04471f87dceb0ca0ee82a
Antonia <f62gogaa@uco.es>
2021-03-31 20:04:11 +0200

    Definicion de posicion y coliscion

--- MC.m
+++ MC.m
@@ -1,28 +1,71 @@
-%Comienzo llamando la función BLOQUE1(), esta función me va a permitir
-%meter los parametros que yo voy a controlar de la simulacion y va a
-%definir algunas variables internas (como constantes o valores fijos que yo
-%necesite). En esta funcion, necesito una funcion que me calcule las
-%velocidades iniciales siguiendo una función de distribuición Maxwelliana.
-BLOQUE1()
-%Calculo el salto de tiempo.
-t=t(E_v,T,m,R);
-for w=1:1:part
-    %Cacula la posicion donde esta mi particula cuando llega el momento de
-    %la colision.
-    [r,v]=posicion(r0,v0,t,F,m);
-    %Ahora calculo la energía que tiene mi particula para calcula el tipo
-    %de colision.
-    E=energia(v,m);
-    %Ahora calculo el tipo de colision que se da.
-    tipocolision=tipocolision(E);
-    switch tipocolision
-        case 1
-            [v_n,E_n]=elastica(v,E);
-        case 2
-            [v_n,E_n]=inelastica(v,E);
-        case 3
-            v_n=v;
-            E_n=E;
+%Comienzo limpiando el Workspace
+clear
+%Empiezo a definir las variables necesarias a lo largo del programa.
+[P,T,E,r0,t0,E_max0,part,R,inter_max]=parametrosexternos();
+[m,M,n,F,E_v,v0,r_0]=parametrosfijos(r0,P,T,E,E_max0,part,R);
+%Calculo la maxima seccion:
+[max_seccion]=t(E_v,n,v0);
+%Inicia un bucle que calcula la posicion, velocidad, energia, colisiones
+%para todas las particulas cada interacción:
+for w=1:1:inter_max
+    %Primero elimino el valor de la variable t
+    clear t
+    if w==1
+        %Calculo el tiempo para la primera interaccion
+        [t]=t(E_v,n,v0);
+        tT=[t0 t];
+        tI=sum(tT);
+        %Calculo la posicion y velocidad para la primera interacción.
+        [r,v]=posicion(r_0,v0,tI,F,m,part);
+        clear tI
+        %Calculo la energía de las paticulas:
+        [E_T]=energia(v,m,part);
+        %Calculo tipo de colision por particula y su nueva velocidad.
+        for p=1:1:part
+            %Tipo colision
+            [tp]=tipocolision(E(p,:),max_seccion);
+            %Colision
+            switch tp
+                case 1
+                    [v_n(:,p),E_T_n(:,p)]=elastica(v(:,p),m,M);
+                case 2
+                    [v_n(:,p),E_T_n(:,p)]=inelastica(v(:,p),m,E(:,p));
+                case 3
+                    v_n(:,p)=v(:,p);
+                    E_T_n(:,p)=E_T;
+            end
+        end
+        %Guardamos la primera interacción:
+        R_T_I=[r];
+        V_T_I=[v_n];
+        %Guarda la energía media de todo el sistema
+        E_T_m=[mean(E_T_n)];     
+    else
+        %Calculamos el tiempo que avanza cada interacción, indices de los for
+        %dentro j,l
+        [t]=t(E_v,n,v);
+        tT=[tT t];
+        tI=sum(tT);
+        %Calculamos la posicion y la velocidad de las particulas.
+        [r,v]=posicion(r,v,tI,F,m,part);
+        clear tI
+        %Calculamos la energía de las particulas:
+        [E_T]=energia(v,m,part);
+        %Calculo tipo de colision por particula y su nueva velocidad.
+        for p=1:1:part
+            %Tipo colision
+            [tp]=tipocolision(E(p,:),max_seccion);
+            %Colision
+            switch tp
+                case 1
+                    [v_n(:,p),E_T_n(:,p)]=elastica(v(:,p),m,M);
+                case 2
+                    [v_n(:,p),E_T_n(:,p)]=inelastica(v(:,p),m,E(:,p));
+                case 3
+                    v_n(:,p)=v(:,p);
+                    E_T_n(:,p)=E_T;
+            end
+        end
     end
     %Guardo en cada columna los datos de cada particula, y en cada fila una
     %coordenada de mi sistema de 3D.
@@ -30,7 +73,6 @@ for w=1:1:part
     V_T(:,w)=v_n';
     E_T(:,w)=E_v;
 end
-%Redifinir las condiciones iniciales de las particulas para cada una de
-%ella.
+
 
 
