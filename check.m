46af1ba28ee7226c64ea898ba094daad67806081
Antonia <f62gogaa@uco.es>
2021-04-07 22:48:20 +0200

    De forma relativa, limite pedir en porcentaje

--- check.m
+++ check.m
@@ -1,5 +1,6 @@
 function [chk]=check(E0,EI,intermax,inter,limite,chk)
-    if inter<intermax && (EI-E0)<limite && chk==1
+    Em=(E0+EI)/2;
+    if inter<intermax && (EI-E0)/Em<limite && chk==1
         %En este caso no hemos llegado a las interacciones maximas y la
         %diferencia entre la media de la energía de todas las particulas de
         %la interacción anterior y de esta interacción es menos que el
@@ -7,7 +8,7 @@ function [chk]=check(E0,EI,intermax,inter,limite,chk)
         %a volver a hacer un bucle mas de interacciones y representamos los
         %datos
         chk=1;
-    elseif inter<intermax && (EI-E0)>limite
+    elseif inter<intermax && (EI-E0)/Em>limite
         %En este caso no hemos llegado a las interacciones maximas y la
         %diferencia entre la media de la energía de las particulas de la
         %interaccion anterior y de la actual interaccion es mayor que el
