f03b4cfb73beec309a4c43e48505387b9b6f7dbf
Antonia <f62gogaa@uco.es>
2021-04-12 21:04:59 +0200

    Unidades

--- parametrosfijos.m
+++ parametrosfijos.m
@@ -1,7 +1,7 @@
 function [m,M,n,F,E_v,v0,r_0]=parametrosfijos(r0,P,T,E,E_max0,part,R)
-    %masa electron
+    %masa electron en kg
     m=9.1*10^-31;
-    %masa particula neutra
+    %masa particula neutra en kg
     M=10000*m;
     %Densidad particula neutras
     n=P/(R*T);
@@ -9,14 +9,14 @@ function [m,M,n,F,E_v,v0,r_0]=parametrosfijos(r0,P,T,E,E_max0,part,R)
     q=1;
     %Fuerza que se aplica al sistema.
     F=q.*E;
-    %Vector de energia para calcular t.
+    %Vector de energia para calcular t en eV.
     E_v=(0.01:0.01:E_max0);
-    %Velocidad inicial.
+    %Velocidad inicial m/s.
     v0=zeros(3,part);
         for w=1:1:part
-            v0(:,w)=[rand rand rand];
+            v0(:,w)=[rand rand rand].*1E22;
         end
-    %Matriz r para iniciar todos desde el mismo punto.
+    %Matriz r para iniciar todos desde el mismo punto en m.
     r_0=zeros(3,part);
         for k=1:1:part
             r_0(:,k)=r0;
