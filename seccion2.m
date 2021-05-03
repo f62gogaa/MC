5f77ac6b63b735932aa065719b3a1078a6aad4ea
Antonia <f62gogaa@uco.es>
2021-05-03 20:25:53 +0200

    cambio seccion

--- seccion2.m
+++ seccion2.m
@@ -4,12 +4,8 @@ function [seccion2]=seccion2(E)
         seccion2=0;
     elseif (10<E)&&(E<=20)
         seccion2=pend*E;
-    elseif (20<E)&&(E<=30)
+    else
         seccion2=10^-23;
-    elseif (30<E)&&(E<=40)
-        seccion2=-pend*E;
-    else 
-        seccion2=0;
     end
 end
         
