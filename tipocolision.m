fcaf866359778cbb36f36c1f8cc2e115a5bdb488
Antonia <f62gogaa@uco.es>
2021-05-03 18:50:24 +0200

    Redefinida completamente

--- tipocolision.m
+++ tipocolision.m
@@ -1,19 +1,30 @@
 function [tipocolision]=tipocolision(E,max_seccion)
-%Esta función calcula la probabilidad que hay de que haya una colision u
-%otra y dictamina de forma aleatoria la colision que se da.
-%Primero calculo la suma de todas las secciones para normalizarla
-    seccion=seccion1(E)+seccion2(E);
-    secciontotal=seccion+(max_seccion-seccion);
-%Calculo vector para estructura y si
-    seccion_cond=[seccion1(E)/secciontotal (seccion1(E)+seccion2(E))/secciontotal 1];
-%Genero numero aleatorio entre 0 y 1
+    %Esta funcion calcula la probabilidad de que halla una colision u otra
+    %y dictamina el tipo de colision que se da de forma aleatoria.
+    %Paso la energía a eV:
+    Es=E/1.60e-19;
+    %Calculo las secciones para el valor de energia E.
+    S1=seccion1(Es);
+    S2=seccion2(Es);
+    S3=max_seccion-S1-S2;
+    %Creo un vector con todas las secciones.
+    seccion_sin=[S1 S2 S3];
+    %Calculo el valor maximo para normalizar
+    SMAX=max(seccion_sin);
+    %Calcula vector con las secciones normalizadas.
+    seccion_cond=seccion_sin./SMAX;
+    %Genero un numero aleatorio que me dictara el tipo de colision que
+    %obtengo.
     random=rand;
-%Estructura y si para seleccionar tipo de colision
-    if (random>=0)&&(random<=seccion_cond(:,1))
+    %Creo estructura de if que me selecciona el tipo de colision:
+    if (random>=0)&&(random<seccion_cond(1))
+        %Colision Elastica
         tipocolision=1;
-    elseif (random>seccion_cond(:,1))&&(random<=seccion_cond(:,2))
+    elseif (random>seccion_cond(1))&&(random<(seccion_cond(1)+seccion_cond(2)))
+        %Colision Inelastica
         tipocolision=2;
     else
+        %Colision Nula
         tipocolision=3;
     end
 end
