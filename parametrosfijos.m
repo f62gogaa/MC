0d65626bb3940b34a277bf81bffa361e59afc083
Antonia <f62gogaa@uco.es>
2021-05-04 09:45:32 +0200

    Cambio en la velocidad y la fuerza

--- parametrosfijos.m
+++ parametrosfijos.m
@@ -8,13 +8,13 @@ function [m,M,n,F,E_v,v0,r_0]=parametrosfijos(r0,P,T,E,E_max0,part,R)
     %Carga del electrón, en eV.
     q=1.6e-19;
     %Fuerza que se aplica al sistema.
-    F=q.*E;
+    F=(q*n).*E;
     %Vector de energia para calcular t en eV.
     E_v=(0.01:0.01:E_max0);
     %Velocidad inicial m/s.
     v0=zeros(3,part);
         for w=1:1:part
-            v0(:,w)=[rand rand rand].*1E4;
+            v0(:,w)=[rand rand 6].*1E4;
         end
     %Matriz r para iniciar todos desde el mismo punto en m.
     r_0=zeros(3,part);
