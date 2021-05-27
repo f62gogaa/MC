037f4838ecc41a2f2e75d69039971137e6a135dc
Antonia <f62gogaa@uco.es>
2021-05-27 12:57:19 +0200

    Generaciones de datos

--- MC.m
+++ MC.m
@@ -102,102 +102,65 @@ for w=1:inter_max
     avan=input('Energía estable: YES=1 OR NO=0 ');
     switch avan
         case 1 %ENERGIA ESTABLE
+            %Pido nuevo número máximo de interacciones:
+            clear inter_max
+            inter_max=input('Numero de interacciones para recolectar datos ');
+            %Predefino vectores y matrices que van a ser necesarios:
+            POSICIONES=zeros(3,part,inter_max);
+            VELOCIDADES=zeros(3,part,inter_max);
+            %Diferencia entre número de interacciones máximas:
+            anadir=yfinal+inter_max-length(E_allinteraciones);
+            if anadir>0
+                TIEMPO=[tT zeros(1,anadir)];
+                ENERGIA_ALLINTER=[E_allinteraciones zeros(1,anadir)];
+            else
+                TIEMPO=tT;
+                ENERGIA_ALLINTER=E_allinteraciones;
+            end
+            %GENERACION DE DATOS:
+            for b=yfinal:inter_max
+                %Calculo el salto de tiempo:
+                dt=time(max_seccion,vfinal,n,part);
+                %Calculo la nueva posicion y velocidad:
+                [r,v]=posicion(rfinal,vfinal,dt,F,m,part);
+                %Calculo la energia de las particulas:
+                E_allparticulas=energia(v,m,part);
+                %COLISION:
+                %Predefino vnueva Enueva:
+                d=0;
+                vnueva=zeros(3,part);
+                Enueva=zeros(1,part);
+                for g=1:part
+                    %Tipo de colision:
+                    tp=tipocolision(E_allparticulas(g),max_seccion);
+                        %Colision:
+                        switch tp
+                            %Colision elastica:
+                            case 1
+                                [vnueva(:,g),Enueva(g)]=elastica(v(:,g),m,M);
+                            %Colision inelastica:
+                            case 2
+                                [vnueva(:,g),Enueva(g)]=inelastica(v(:,g),m,E_allparticulas(g));
+                            %Colsion nula:
+                            case 3
+                                vnueva(:,g)=v(:,g);
+                                Enueva(g)=E_allparticulas(g);
+                        end %Fin SWITCH PARTICULAS COLISION
+                        %GUARDAR ENERGIA:
+                        ENERGIA_ALLINTER(g,b)=Enueva(g);
+                end %Fin BUCLE PARTICULAS COLISION
+            %GUARDAR ENERGIA MEDIA,TIEMPO,POSICION,VELOCIDAD:
+            TIEMPO(b+1)=TIEMPO(b)+dt;
+            d=d+1;
+            POSICIONES(:,:,d)=r;
+            VELOCIDADES(:,:,d)=vnueva;
+            end %Fin BUCLE GENERACIÓN DE DATOS
             
         case 2 %ENERGÍA INESTABLE
-            clear avan rfinal vfinal
+            clear avan rfinal vfinal yfinal
     end
     %Fin ESTABILIDAD ENERGÍA
     end %Fin BUCLE REPRESENTACIÓN ENERGIA VS INTERACCION.
-end %Fin BUCLE INTERACCIONES
-%GENERACION DE DATOS:
-%Vuelo a pedir numero maximo de interacciones
-clear inter_max
-inter_max=input('Nuevo numero de interaciones maximas ');
-%Pido cauntos datos quiero guardar datos:
-nsave=input('Guardar _ datos de posicion, valocidad ');
-%Predefino matrices que guardaran mis datos:
-%numero de matrices a guardar:
-z=inter_max/nsave;
-Z=floor(z);
-posiciones=zeros(3,part,Z);
-velocidades=zeros(3,part,Z);
-%Repito bucle anterior de interaciones pero con la opcion de guardar:
-for w=1:inter_max
-    %Primera interaccion:
-    if w==1
-        %Calculo el primer salto de tiempo:
-        dt=time(max_seccion,v,n,part);
-        t=t0+dt;
-        %Calculo la nueva posicion y velocidad:
-        [r,v]=posicion(r_0,v0,t,F,m,part);
-        %Calculo la energía de las particulas:
-        E_allparticulas=energia(v,m,part);
-        %COLISION:
-        %Predefino vnueva Enueva:
-        vnueva=zeros(3,part);
-        Enueva=zeros(1,part);
-        for g=1:part
-            %Tipo de colision:
-            tp=tipocolision(E_allparticulas(g),max_seccion);
-            %Colision:
-            switch tp
-                %Colision Elastica:
-                case 1
-                    [vnueva(:,g),Enueva(g)]=elastica(v(:,g),m,M);
-                %Colision Inelastica
-                case 2
-                    [vnueva(:,g),Enueva(g)]=ineslastica(v(:,g),m,E_allparticulas(g));
-                %Colision Nula
-                case 3
-                    vnueva(:,g)=v(:,g);
-                    Enueva(g)=E_allparticulas(g);
-            end %Fin SWITCH COLISION
-            %GUARDAR ENERGIA:
-            E_allinteraciones(g,w)=Enueva(g);
-        end %Fin BUCLE PARTICULAS COLISION
-        %GUARDAR ENERGIA MEDIA Y TIEMPO:
-        tT(w+1)=tT(w)+dt;
-        E_plot(w+1)=mean(E_allinteraciones(:,w));
-        %Demás interaccion:
-    else
-        %Calculo el salto de tiempo:
-        dt=time(max_seccion,vnueva,n,part);
-        %Calculo la nueva posicion y velocidad:
-        [r,v]=posicion(r,vnueva,dt,F,m,part);
-        %Calculo la energía de las particulas:
-        E_allparticulas=energia(v,m,part);
-        %COLISION:
-        %Predefino vnueva Enueva:
-           vnueva=zeros(3,part);
-           Enueva=zeros(1,part);
-        for g=1:part
-            %Tipo de colision:
-            tp=tipocolision(E_allparticulas(g),max_seccion);
-            %Colision:
-            switch tp
-                %Colision Elastica:
-                case 1
-                    [vnueva(:,g),Enueva(g)]=elastica(v(:,g),m,M);
-                %Colision Inelastica
-                case 2
-                    [vnueva(:,g),Enueva(g)]=ineslastica(v(:,g),m,E_allparticulas(g));
-                %Colision Nula
-                case 3
-                    vnueva(:,g)=v(:,g);
-                    Enueva(g)=E_allparticulas(g);
-            end %Fin SWITCH PARTICULAS COLISION
-            %GUARDAR ENERGIA:
-            E_allinteraciones(g,w)=Enueva(g);
-        end %Fin BUCLE PARTICULAS COLISION
-        %GUARDAR ENERGIA MEDIA Y TIEMPO:
-        tT(w+1)=tT(w)+dt;
-        E_plot(w+1)=mean(E_allinteraciones(:,w));
-    end %Fin BUCLE DIFERENCIANDO PRIMERA DE LAS DEMAS INTERACIONES
-    %GUARDAR DATOS:
-    for c=1:nsave:w
-        posiciones(:,:,c)=r;
-        velocidades(:,:,c)=vnueva;
-    end %Fin BUCLE GUARDAR CADA X INTERACIONES
 end %Fin BUCLE INTERACCIONES
 %GENERACION DE ARCHIVOS:
 
