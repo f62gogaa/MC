4f62101b3053ad7d9bcc7c6fdf41488bf3480f85
Antonia <f62gogaa@uco.es>
2021-06-22 13:14:48 +0200

    Cambio seccion eficaz inelastica

--- seccion2.m
+++ seccion2.m
@@ -1,11 +1,15 @@
 function [seccion2]=seccion2(E)
-    pend=1e-24;
+    pend=1e-22;
     if (0<=E)&&(E<=10)
         seccion2=0;
     elseif (10<E)&&(E<=20)
         seccion2=pend*E-10*pend;
+    elseif (20<E)&&(E<=30)
+        seccion2=10^-21;
+    elseif (30<E)&&(E<=40)
+        seccion2=-pend*E+40*pend;
     else
-        seccion2=10^-23;
+        seccion2=0;
     end
 end
         
